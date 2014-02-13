5d6df8001945118c26bde85e9eabe5c951a26832
Ruchi Varshney <rvarshney@hearsaycorp.com>
2014-02-13 08:45:48 -0800

    Hook up shipping view

--- Gift/Gift/AlbumViewController.m
+++ Gift/Gift/AlbumViewController.m
@@ -8,6 +8,7 @@
 
 #import "AlbumViewController.h"
 #import "EmailHelper.h"
+#import "ShippingViewController.h"
 
 @interface AlbumViewController ()
 
@@ -28,15 +29,22 @@
 {
     [super viewDidLoad];
     
+    if ([self respondsToSelector:@selector(edgesForExtendedLayout)]) {
+        self.edgesForExtendedLayout = UIRectEdgeNone;
+    }
+
     // Add email navigation bar button
     UIBarButtonItem *emailButton = [[UIBarButtonItem alloc] initWithTitle:@"Email" style:UIBarButtonItemStyleBordered target:self action:@selector(emailButtonHandler:)];
-    self.navigationItem.rightBarButtonItem = emailButton;
+
+    // Add print navigation bar button
+    UIBarButtonItem *printButton = [[UIBarButtonItem alloc] initWithTitle:@"Print" style:UIBarButtonItemStyleBordered target:self action:@selector(printButtonHandler:)];
+
+    self.navigationItem.rightBarButtonItems = [NSArray arrayWithObjects:emailButton, printButton, nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 - (void)emailButtonHandler:(id)sender
@@ -44,36 +52,41 @@
     NSError *error = nil;
     BOOL isEmailSent = [EmailHelper sendEmailWithNavigationController:self.navigationController subject:@"My Cool Album" to:[NSArray arrayWithObject:@""] cc:nil bcc:nil body:@"My Cool Album!" isHTML:YES delegate:self files:[NSArray arrayWithObjects:@"ronnie-header-2.pdf",nil] error:&error];
     
-    if(!isEmailSent)
+    if (!isEmailSent) {
         NSLog(@"Failed with error: %@",error);
+    }
 }
 
--(void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error{
-    NSLog(@"didFinishWithResult start");
-    
+- (void)printButtonHandler:(id)sender
+{
+    ShippingViewController *shippingViewController = [[ShippingViewController alloc] init];
+    [self.navigationController pushViewController:shippingViewController animated:YES];
+}
+
+-(void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
     switch (result) {
         case MFMailComposeResultCancelled:
             NSLog(@"MFMailComposeResultCancelled");
             break;
-            
+
         case MFMailComposeResultSaved:
             NSLog(@"MFMailComposeResultSaved");
             break;
-            
+
         case MFMailComposeResultFailed:
             NSLog(@"MFMailComposeResultFailed");
             break;
-            
+
         case MFMailComposeResultSent:
             NSLog(@"MFMailComposeResultSent");
             break;
-            
+
         default:
             break;
-            
     }
     
-    // Close the Mail Interface
+    // Close the mail interface
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
